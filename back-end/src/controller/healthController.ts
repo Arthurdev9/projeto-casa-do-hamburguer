fdae1bdf3efc654763bd2a0e66686c31142ba0a2
Arthur Vinicius <arthurvinicius081@hotmail.com>
2026-02-21 15:58:30 -0300

    refactor: streamline healthcheck response for better performance

--- back-end/src/controller/healthController.ts
+++ back-end/src/controller/healthController.ts
@@ -4,8 +4,8 @@ import { type Request, type Response } from 'express'
 export const healthCheck = async (req: Request, res: Response) => {
   try {
     await prisma.$queryRaw`SELECT 1`
-    res.status(200).json({ status: 'OK', message: 'Back and DB are warm!' })
+    res.sendStatus(200)
   } catch (err) {
-    res.status(500).json({ status: 'Error', message: 'DB is cold' })
+    res.sendStatus(500)
   }
 }
